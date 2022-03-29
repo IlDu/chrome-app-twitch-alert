c8e928b524da75464fe18502a7d102c90114fb7c
IlDu <102491993+IlDu@users.noreply.github.com>
2022-03-29 23:50:53 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var config = {
-  name: "DrDisRespect",
+  name: "Lynka",
   clientID: "YOUR_CLIENT_ID", // How generate client ID ? -> https://docs.aws.amazon.com/lumberyard/latest/userguide/chatplay-generate-twitch-client-id.html
   default_color: "#c23131"
 }
